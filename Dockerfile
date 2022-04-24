33c62254e6460a49a18c188c704ff129e1f97cea
Vinh Phan <pvphan90@gmail.com>
2022-04-24 11:05:26 -0700

    figuring out conda install and dependencies for ampere

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM anibali/pytorch:1.8.1-cuda11.1
 
 WORKDIR /RAFTStereo
 COPY environment.yaml ./environment.yaml
-RUN conda env create -f environment.yaml
+RUN conda env create -f environment.yaml && conda init bash
 RUN echo "conda activate myenv" >> ~/.bashrc
 SHELL ["/bin/bash", "--login", "-c"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,4 +14,5 @@ conda activate raftstereo
 # Re-enable strict mode:
 set -euo pipefail
 
+bash
 #exec python demo.py --restore_ckpt models/raftstereo-middlebury.pth --corr_implementation alt --mixed_precision -l=datasets/Middlebury/MiddEval3/testF/*/im0.png -r=datasets/Middlebury/MiddEval3/testF/*/im1.png
